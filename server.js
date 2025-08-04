5714295035f10b6e56c336977af37a41835f00f2
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-08-05 00:30:34 +0530

    fix: Correct error handling responses for upload and download URL endpoints

--- server.js
+++ server.js
@@ -7,6 +7,24 @@
  app.get("/upload-url",(req,res)=>{
     const {filename ,contentType} =req.query;
     if(!filename||!contentType){
-        return res.status(400)
+        return res.status(400).json({error:'filename and content type is require'})
     }
+
+    const key =`uploads/${filename}`;
+    const url= generateSignedUrl({method:"PUT",key,contentType});
+    res.json({url,key});
+ })
+
+
+ app.get("/download-url",(req,res)=>{
+    const {key} =req.query;
+    if(!key){
+        return res.status(400).json({error:"Key is require"})
+    }
+    const url= generateSignedUrl({method:"GET",key});
+    res.json({url});
+ })
+
+ app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
  })
