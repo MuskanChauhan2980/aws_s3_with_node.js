348b2c7c5191c9a0739e753467dad04f07ba4cd3
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-08-04 21:35:30 +0530

    format code for consistency and readability

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const {S3Client,GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
- 
-const s3Client=new S3Client({
-    region:"ap-south-1",
-    credentials:{
-        accessKeyId:"",
-        secretAccessKey:""
+
+const s3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: "",
+        secretAccessKey: ""
     }
 });
 
-async function getObjectUrl(key){
- const command=new GetObjectCommand({
-    Bucket:"Bucket_Name",
-    Key:key
- });
- const url=await  getSignedUrl(s3Client,command);
- return url;
+async function getObjectUrl(key) {
+    const command = new GetObjectCommand({
+        Bucket: "Bucket_Name",
+        Key: key
+    });
+
+    const url = await getSignedUrl(s3Client, command);
+    return url;
 }
 
-async function init(){
-console.log("URL for graphql.jpeg", await getObjectUrl("graphql.jpeg"))
+async function init() {
+    console.log("URL for graphql.jpeg", await getObjectUrl("graphql.jpeg"))
 }
 
 init();
