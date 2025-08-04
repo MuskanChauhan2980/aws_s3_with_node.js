2de047bdabe3c57b0ad75c4f27abe56b42176416
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-08-04 23:01:27 +0530

    refactor: organize imports and enhance URL retrieval functions

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client,
+    GetObjectCommand,
+    putObjectUrl,
+    ListObjectsV2Command } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const s3Client = new S3Client({
     region: "ap-south-1",
     credentials: {
-        accessKeyId: "",
-        secretAccessKey: ""
+        accessKeyId: "",   // access key in aws account 
+        secretAccessKey: ""  // secret accesss key in aws account 
     }
 });
 
 async function getObjectUrl(key) {
     const command = new GetObjectCommand({
-        Bucket: "Bucket_Name",
+        Bucket: "Bucket_Name",     // Bucket name which is we are use in aws account 
         Key: key
     });
 
+    const url = await getSignedUrl(s3Client, command);   // create a presigned url by using the s3Client and  getObjectcommand 
+    return url;
+}
+
+async function putObjectUrl(fielName, contentType) {
+    const command = new putObjectUrl({
+        Bucket: "BucketName-private",
+        Key: `/uploads/user-uploads/${fielName}`,
+        contentType: contentType
+    })
     const url = await getSignedUrl(s3Client, command);
     return url;
 }
@@ -22,6 +35,12 @@ async function getObjectUrl(key) {
 async function init() {
     console.log("URL for graphql.jpeg", await getObjectUrl("graphql.jpeg"))
 }
+async function initPutObjectUrl() {
+    console.log("URL for uploading", await putObjectUrl(`image-${Date.now()}.jpeg`, "imge/jpeg"))
+}
 
 init();
+initPutObjectUrl();
+
+
 
