ff2190e5b1dba7aa60c2e3410f7eabad15118a4b
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-08-05 22:23:32 +0530

    refactor: Simplify server setup and integrate S3 routes

--- server.js
+++ server.js
@@ -1,30 +1,12 @@
- const express=require('express');
- const {generateSignedUrl} =require('./s3');
- require("dotenv").config();
- const app=express();
- const PORT=3000;
+ const express= require("express");
+ const cors= require("cors");
+ const s3Routes= require("./route/s3Router");
 
- app.get("/upload-url",(req,res)=>{
-    const {filename ,contentType} =req.query;
-    if(!filename||!contentType){
-        return res.status(400).json({error:'filename and content type is require'})
-    }
-
-    const key =`uploads/${filename}`;
-    const url= generateSignedUrl({method:"PUT",key,contentType});
-    res.json({url,key});
- })
-
-
- app.get("/download-url",(req,res)=>{
-    const {key} =req.query;
-    if(!key){
-        return res.status(400).json({error:"Key is require"})
-    }
-    const url= generateSignedUrl({method:"GET",key});
-    res.json({url});
- })
-
+ const app= express();
+ app.use(cors());
+ app.use(express.json());
+ app.use("/s3", s3Routes);
+ const PORT = process.env.PORT ||3000;
  app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
+   console.log(`Server runing on http://localhost:${PORT}`)
  })
