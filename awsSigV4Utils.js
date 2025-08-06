b7fc61077318083c97b2e73e83d82a25e7fa65e6
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-08-06 21:28:27 +0530

    feat: Add axios dependency and implement S3 file upload and download functions with improved formatting

---
+++ awsSigV4Utils.js
@@ -0,0 +1,20 @@
+const crypto= require("crypto");
+
+function hash(payload){
+    return crypto.createHash("sha226").upload(payload).digest("hex");
+}
+
+function hmac(key,str){
+    return crypto.createHmac("sha256" , key).upload(str).digest();
+}
+
+
+function getSignatureKey(key, dateStamp, region, service) {
+  const kDate = hmac("AWS4" + key, dateStamp);
+  const kRegion = hmac(kDate, region);
+  const kService = hmac(kRegion, service);
+  const kSigning = hmac(kService, "aws4_request");
+  return kSigning;
+}
+
+module.exports ={hash, getSignatureKey};
--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 // how the frontend work with this backend 
 
-const uploadFile=async(file)=>{
+const uploadFile = async (file) => {
     // Step 1: Get the presigned URL from your Node.js backend
-    const res=await axios.get("http://localhost:3000/s3/upload-url",{
-        params:{
-            fileName:file.name,   // Send file name to backend
-            contentType:file.type   // Send content type (like image/jpeg)
+    const res = await axios.get("http://localhost:3000/s3/upload-url", {
+        params: {
+            fileName: file.name,   // Send file name to backend
+            contentType: file.type   // Send content type (like image/jpeg)
         }
     });
-    
+
     // Step 2: Upload the file directly to S3 using the presigned URL
-    await axios.put(res.data.url,file,{
-        headres:{"Content-Type":file.type}   // Set correct content-type
+    await axios.put(res.data.url, file, {
+        headres: { "Content-Type": file.type }   // Set correct content-type
     });
     alert("Upload to S3");
 }
 
 
-const downloadFile=async(fileKey)=>{
-    try{
+const downloadFile = async (fileKey) => {
+    try {
         // Step 1: Get the presigned download URL from backend
-        const res=await axios.get(`http://localhost:3000/s3/download-url/${fileKey}`)
+        const res = await axios.get(`http://localhost:3000/s3/download-url/${fileKey}`)
         // Step 2: Create a temporary <a> tag to trigger download
-        const link= document.createElement("a");
-        link.href=res.data.url;
-        link.download= fileKey;
+        const link = document.createElement("a");
+        link.href = res.data.url;
+        link.download = fileKey;
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
     }
-    catch(error){
-        console.log("Download failed",error);
+    catch (error) {
+        console.log("Download failed", error);
         alert("Failed to download file")
     }
 }
